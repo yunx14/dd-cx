a4d6cdc63e0f10014e5c154b78a95d378b24ead0
Robert T Warren <RWarren@delta.org>
2017-04-12 15:02:28 -0700

    adding JSDoc for all classes

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# Simple Service
-Simple service based on express
+# Provider Directory Experience Endpoint
+An express-based Experience endpoint solely for use on HTML-based clients
 
+
+## API
+
+### /location-search.html
+Searches the Directory by zip code
--- collections/collection.js
+++ collections/collection.js
@@ -1,13 +1,53 @@
 // Abstract Collection
 var http = require("http");
 
+/**
+ * A collection class for use of holding and retrieving models
+ * @constructor Collection
+ * @example var collection = new Collection();
+ */
 module.exports = Collection = function() {
   return {
+    /**
+     * @property host {string} host for use in the Collection's fetch method
+     * @memberof Collection
+     */
     "host": "",
+    /**
+     * @property path {string} path for use in the Collection's fetch method
+     * @memberof Collection
+     */
     "path": "",
+    /**
+     * @property port {number} port for use in the Collection's fetch method
+     * @memberof Collection
+     */
     "port": 0,
+    /**
+     * @property attributes {object} The data of the Collection
+     * @memberof Collection
+     */
     "attributes": {},
+    /**
+     * @property query {object} The query params for use in REST calls
+     * @memberof Collection
+     */
     "query": {},
+    /**
+     * @method getURI returns the formatted URI from local properties
+     * @returns {string} URI of the model for use in REST calls
+     * @memberof Collection
+     */
+    "getURI": function() {
+      return this.host + ":" + String(this.port) + this.path;
+    },
+    /**
+     * @method fetch Fetches data from a remove URI and populates this collection
+     * @param options {object} Options to pass to the remove call
+     * @param success {function} Success ballback
+     * @param error {function} Error callback
+     * @memberof Collection
+     */
     "fetch": function(options, success, error) {
       if (!options) {
         options = {};
@@ -59,15 +99,36 @@ module.exports = Collection = function() {
         error(e);
       });
     },
+    /**
+     * @method toString Returns a string representation of the data
+     * @returns {string} string representation of the data
+     * @memberof Collection
+     */
     "toString": function() {
       return JSON.stringify(this.attributes);
     },
+    /**
+     * @method index Returns a model at a specified index
+     * @param i {number} The index of the model
+     * @returns {any} a model at a specified index
+     * @memberof Collection
+     */
     "index": function(i) {
       return (i) ? this.attributes[i] : null;
     },
+    /**
+     * @method toJSON Returns a JSON representation of the data
+     * @returns {object} JSON representation of the data
+     * @memberof Collection
+     */
     "toJSON": function() {
       return this.attributes;
     },
+    /**
+     * @method set Sets the collection data
+     * @param data {array} The data to set
+     * @memberof Collection
+     */
     "set": function(data) {
       if (data) {
         this.attributes = data;
