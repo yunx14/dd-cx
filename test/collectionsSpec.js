dfd934fbfc6c3e2519d512993f35806793e13284
Joanne Xin <JXin@delta.org>
2017-04-13 09:20:54 -0700

    [add] more spec tests for collection

--- test/collectionsSpec.js
+++ test/collectionsSpec.js
@@ -5,25 +5,25 @@ describe("Given Locations Collection", function() {
 		expect(Collection).toBeDefined();
 	});
 
-  it("can create an object", function() {
-    var collection = new Collection();
+	it("can create an object", function() {
+		var collection = new Collection();
 		expect(collection).toBeDefined();
 	});
 
 	it("has all the properties of the object", function() {
-    var collection = new Collection();
+		var collection = new Collection();
 		var actualProperties = Object.keys(collection).sort();
 		var expectedProperties = [
-		  'host',
-		  'path',
+			'host',
+			'path',
 			'port',
-		  'attributes',
+			'attributes',
 			'query',
-		  'getURI',
+			'getURI',
 			'fetch',
-		  'toString',
+			'toString',
 			'index',
-		  'toJSON',
+			'toJSON',
 			'set'
 		].sort();
 		expect(actualProperties).toEqual(expectedProperties);
@@ -48,58 +48,58 @@ describe("Given Locations Collection", function() {
 
 	it("matches objects with the expect key/value pairs", function() {
 		var collection = new Collection();
-    expect(collection).toEqual(jasmine.objectContaining({
-      attributes: {}
-    }));
-    expect(collection).toEqual(jasmine.objectContaining({
-      query: {}
-    }));
-  });
+		expect(collection).toEqual(jasmine.objectContaining({
+			attributes: {}
+		}));
+		expect(collection).toEqual(jasmine.objectContaining({
+			query: {}
+		}));
+	});
 });
 
 describe("As a collection", function() {
-  var collection = null;
+	var collection = null;
 
-  beforeEach(function() {
-    collection = new Collection();
-  });
+	beforeEach(function() {
+		collection = new Collection();
+	});
 
 	afterEach(function() {
-    collection = null;
-  });
+		collection = null;
+	});
 
 	it("can stringify data", function() {
 		var result = collection.toString();
-    expect(JSON.stringify(collection.attributes)).toEqual(result);
-  });
+		expect(JSON.stringify(collection.attributes)).toEqual(result);
+	});
 
-  it("can return JSON data", function() {
+	it("can return JSON data", function() {
 		var result = collection.toJSON();
-    expect(collection.attributes).toEqual(result);
-  });
+		expect(collection.attributes).toEqual(result);
+	});
 
 	it("can retrieve data through index", function() {
 		var result = collection.index(1);
-    expect(collection.attributes[1]).toEqual(result);
-  });
+		expect(collection.attributes[1]).toEqual(result);
+	});
 
 });
 
 describe("As a collection", function() {
-  var collection = null;
-  var result = ["I am a test", 123];
+	var collection = null;
+	var result = ["I am a test", 123];
 
-  beforeEach(function() {
-    collection = new Collection();
-  });
+	beforeEach(function() {
+		collection = new Collection();
+	});
 
 	afterEach(function() {
-    collection = null;
-  });
+		collection = null;
+	});
 
-  it("can set data", function() {
+	it("can set data", function() {
 		collection.set(["I am a test", 123]);
-    expect(collection.attributes).toEqual(result);
-  });
+		expect(collection.attributes).toEqual(result);
+	});
 
 });
