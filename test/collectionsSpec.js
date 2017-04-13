92c6f77ffe1af5751a3cefa3017a16f00597071b
Joanne Xin <JXin@delta.org>
2017-04-13 09:18:40 -0700

    [add] more spec tests for collection

--- test/collectionsSpec.js
+++ test/collectionsSpec.js
@@ -10,6 +10,25 @@ describe("Given Locations Collection", function() {
 		expect(collection).toBeDefined();
 	});
 
+	it("has all the properties of the object", function() {
+    var collection = new Collection();
+		var actualProperties = Object.keys(collection).sort();
+		var expectedProperties = [
+		  'host',
+		  'path',
+			'port',
+		  'attributes',
+			'query',
+		  'getURI',
+			'fetch',
+		  'toString',
+			'index',
+		  'toJSON',
+			'set'
+		].sort();
+		expect(actualProperties).toEqual(expectedProperties);
+	});
+
 	it("functions are defined", function() {
 		var collection = new Collection();
 		expect(collection.fetch).toBeDefined();
@@ -17,6 +36,14 @@ describe("Given Locations Collection", function() {
 		expect(collection.index).toBeDefined();
 		expect(collection.toJSON).toBeDefined();
 		expect(collection.set).toBeDefined();
+	});
+
+	it("matches typeof the object properties", function() {
+		var collection = new Collection();
+		expect(typeof collection.host).toEqual('string');
+		expect(typeof collection.port).toEqual('number');
+		expect(typeof collection.getURI).toEqual('function');
+		expect(typeof collection.attributes).toEqual('object');
 	});
 
 	it("matches objects with the expect key/value pairs", function() {
@@ -28,6 +55,34 @@ describe("Given Locations Collection", function() {
       query: {}
     }));
   });
+});
+
+describe("As a collection", function() {
+  var collection = null;
+
+  beforeEach(function() {
+    collection = new Collection();
+  });
+
+	afterEach(function() {
+    collection = null;
+  });
+
+	it("can stringify data", function() {
+		var result = collection.toString();
+    expect(JSON.stringify(collection.attributes)).toEqual(result);
+  });
+
+  it("can return JSON data", function() {
+		var result = collection.toJSON();
+    expect(collection.attributes).toEqual(result);
+  });
+
+	it("can retrieve data through index", function() {
+		var result = collection.index(1);
+    expect(collection.attributes[1]).toEqual(result);
+  });
+
 });
 
 describe("As a collection", function() {
