9a51c2ab4c8aca97c6d79c37131a97d5539dce35
Robert T Warren <RWarren@delta.org>
2017-04-17 16:50:18 -0700

    integrate live template

---
+++ views/provider-directory-search.js
@@ -0,0 +1,57 @@
+module.exports = {
+  "form" : {
+    "formFields": [
+      {
+        "field-id" : "location",
+        "labelAttribute" : [
+          {
+            "label" : "Location"
+          }
+        ],
+        "inputAttribute" : [
+          {
+            "attributeName" : "type",
+            "attributeValue": "text"
+          },
+          {
+            "attributeName" : "class",
+            "attributeValue": "form-input "
+          },
+          {
+            "attributeName" : "name",
+            "attributeValue": "location"
+          },
+          {
+            "attributeName" : "placeholder",
+            "attributeValue": "Zip Code"
+          },
+          {
+            "attributeName" : "value",
+            "attributeValue": ""
+          }
+        ]
+      }
+    ]
+  },
+  "contact": {
+    "phoneNumber": "844.847.9516"
+  },
+  "footerLinks": [
+    {
+      "linkHref" : "1",
+      "aLink": "Privacy"
+    },
+    {
+      "linkHref" : "2",
+      "aLink": "Legal Notices"
+    },
+    {
+      "linkHref" : "3",
+      "aLink": "Language Assistance"
+    },
+    {
+      "linkHref" : "4",
+      "aLink": "Contact Us"
+    }
+  ]
+};
