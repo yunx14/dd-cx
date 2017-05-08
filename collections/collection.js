f5ea80788ee87aafead8cfb525d310653be66dea
Joanne Xin <JXin@delta.org>
2017-05-08 16:31:07 -0700

    [clean] console.log

--- collections/collection.js
+++ collections/collection.js
@@ -46,23 +46,23 @@ Collection.prototype.getURI = function() {
   return this.host + ":" + String(this.port) + this.path;
 };
 var formatQuery = function(query) {
-  console.log(new Date().getTime());
   var formatQuery = "?", i = 0, keys = Object.keys(query), l = keys.length;
   for (i = 0; i < l; i++) {
     if (typeof query[keys[i]] !== "function" && Array.isArray(query[keys[i]])) {
       var tempVal = query[keys[i]];
+      // for loop method:
       // var result = "";
       // for (var j = 0; j < tempVal.length; j++) {
       //   result = result + String(keys[i]) + "=" + tempVal[j] + "&";
       // }
       // formatQuery = formatQuery + result;
 
+      // array.join method:
       formatQuery = formatQuery + String(keys[i]) + "=" + tempVal.join("&" + String(keys[i]) + "=") + "&";
     } else if (typeof query[keys[i]] !== "function") {
       formatQuery = formatQuery + String(keys[i]) + "=" + String(query[keys[i]]) + "&";
     }
   }
-  console.log(new Date().getTime());
   return formatQuery.slice(0, -1);
 };
 /**
