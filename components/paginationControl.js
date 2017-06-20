161aa981b233c9b13248e373d45fb18ff861d53c
Robert T Warren <RWarren@delta.org>
2017-06-20 13:20:45 -0700

    Move Model to request vs http/https

--- components/paginationControl.js
+++ components/paginationControl.js
@@ -6,12 +6,43 @@
 module.exports = PaginationControl = {
   /**
   * @method render returns formatted array for atomicPower to use for pagination control
+  * @param baseURI {string}
   * @param current {number}
   * @param total {number}
   * @memberof PaginationControl
   * @returns {array} web standards query string
   */
-  render: function(current, total) {
-    return [];
+  render: function(baseURI, current, total) {
+
+
+
+    var vm = [
+     {
+       "label":  "previous",
+       "URI": baseURI + "page=" + current - 1,
+       "current": false
+     },
+     {
+       "label":  "1",
+       "URI": "x",
+       "current": false
+     },
+     {
+       "label":  "2",
+       "URI": "x",
+       "current": true
+     },
+     {
+       "label":  "3",
+       "URI": "x",
+       "current": false
+     },
+     {
+       "label":  "next",
+       "URI": "x",
+       "current": false
+     }
+   ];
+    return vm;
   }
 }
--- models/model.js
+++ models/model.js
@@ -1,8 +1,10 @@
 // abstract model
-var http = require("http");
+//var http = require("http");
 //var logger = require("../utility/logger.js");
 
 //var logger = new Logger();
+
+// now using request
 
 /**
  *  A model class for use of holding and retrieving data
@@ -17,6 +19,13 @@ var Model = function(attributes) {
    */
    this.attributes = (attributes) ? attributes : {};
 };
+
+/**
+ * @property secure {boolean} Secure (SSL) for use in the Collection's fetch method
+ * @memberof Collection
+ */
+Collection.prototype.secure = false;
+
 /**
  * @property host {string} host for use in the model's fetch method
  * @memberof Model
@@ -57,6 +66,95 @@ Model.prototype.getURI = function() {
   * @param error {function} Error callback
   * @memberof Model
   */
+  Model.prototype.fetch = function(options, success, error) {
+    if (!options) {
+      options = {};
+    }
+    if (!options.host) {
+      options.host = this.host;
+    }
+    if (!options.port) {
+      options.port = this.port;
+    }
+    if (!options.path) {
+      options.path = this.path;
+    }
+    if (!options.query) {
+      options.query = this.query;
+    }
+
+    // add any request options defined globally in node
+    if (global_request_options && global_request_options.agentOptions) {
+      options.agentOptions = global_request_options.agentOptions;
+    }
+
+    Logger.debug("Query " + JSON.stringify(options.query));
+    if (!success) {
+      success = function(status, data) {
+        Logger.debug("Fetched Data! " + status);
+      }
+    }
+    if (!error) {
+      error = function(e) {
+        Logger.warn("Failed to fetched Data! " + e);
+      }
+    }
+
+    const uri = options.host + ":" + String(options.port) + options.path + Utils.formatQuery(options.query);
+    Logger.debug("uri " + uri);
+
+    options.url = uri;
+
+    this.secure = (uri.indexOf("https") !== -1);
+
+    Logger.debug("Requesting with options: " + JSON.stringify(options));
+
+    let req = request.get(options, function (err, res, body) {
+      Logger.debug("error " + err);
+      Logger.debug("res " + res);
+      Logger.debug("body " + body);
+
+      let status = 200;
+
+      if (err) {
+        Logger.warn("Exception " + err);
+        status = 500;
+      } else {
+        status = (res && res.statusCode) ? res.statusCode : 200;
+        Logger.debug("STATUS: " + status);
+
+        let data = {};
+        try {
+          data = JSON.parse(body);
+        } catch(e) {
+          Logger.warn("Exception, data was not in expected format: " + e);
+          // TODO: we need to do something with bad server responses that are not expected or formatted correctly
+        }
+        this.attributes = data;
+        Logger.debug("calling success");
+        success(status, data);
+      }
+    });
+
+    req.on("error", function(e) {
+      Logger.debug("Error handler " + e);
+      let data = {};
+      try {
+        if (typeof e === "object") {
+          data = JSON.stringify(e);
+        } else {
+          data = e;
+        }
+      } catch(ex) {
+        Logger.warn("Exception: " + ex);
+        data = {};
+      }
+      Logger.warn("Exception " + JSON.stringify(data));
+      error(500, e);
+    });
+  };
+
+  /*
 Model.prototype.fetch = function(options, success, error) {
   if (!options) {
     options = {};
@@ -110,6 +208,7 @@ Model.prototype.fetch = function(options, success, error) {
     error(e);
   });
 };
+*/
 /**
  * @method toString Returns a string representation of the data
  * @returns {string} string representation of the data
