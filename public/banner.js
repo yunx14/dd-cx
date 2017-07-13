22bf6a3fbdb8cf63a2be46675fcbcbdb78ae2f28
Charles Johnston <cjohnston@delta.org>
2017-07-13 13:16:03 -0700

    banner

--- public/banner.js
+++ public/banner.js
@@ -1,18 +1,36 @@
 
 function createCookie(name, value) {
   var date = new Date();
+  var expires = "";
   date.setTime(date.getTime() + (30 * 60 * 1000));
-  $.cookie(name, value, { expires: date });
+  expires = date.toUTCString();
+  document.cookie = name+"="+value+"; expires="+expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
 
 $(".banner__close").click(function() {
   $(".banner-container").hide();
-  createCookie("showBanner", false);
+  createCookie("showBanner", "no");
 });
 
 $(document).ready(function() {
-  var cookie = $.cookie("showBanner");
-  if(cookie) {
+  var cookie = getCookie("showBanner");
+  if(cookie !== "") {
     $(".banner-container").hide();
   }
 });
