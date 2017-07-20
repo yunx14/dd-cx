4607782f567cc01e94d5f4f954f201c026e54195
Charles Johnston <cjohnston@delta.org>
2017-07-20 10:01:02 -0700

    fix

--- public/helpers.js
+++ public/helpers.js
@@ -1,6 +1,7 @@
 var FEHelpers = (function(){
   var mapPos = document.getElementById("provider-map");
   var formBottom = document.getElementsByClassName("provider-form")[0];
+  var col = document.getElementsByClassName("provider-list")[0];
   var bodyHeight = $("body").height();
   var footerHeight = $(".page-footer").height();
   var mapBottomOffset = 67;
@@ -22,7 +23,10 @@ var FEHelpers = (function(){
 
   var fixmystuff = function() {
     if(formBottom.getBoundingClientRect().bottom <= 0) {
-      if(window.pageYOffset >= (bodyHeight - window.innerHeight - footerHeight - mapBottomOffset)) {
+      if(mapPos.getBoundingClientRect().top > 32) {
+        mapPos.className = "provider-map fixed";
+      }
+      if(Math.ceil(mapPos.getBoundingClientRect().bottom) >= Math.ceil(col.getBoundingClientRect().bottom)) {
         mapPos.className = "provider-map absolute";
       } else {
         mapPos.className = "provider-map fixed";
