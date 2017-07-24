6bc0442ecde64ac72f919fa7bfd7623dbafd2ac8
Robert T Warren <RWarren@delta.org>
2017-07-24 11:54:50 -0700

    remove debug

--- server/mainHelpers.js
+++ server/mainHelpers.js
@@ -409,7 +409,6 @@ var getListsResults = function(query, req, res) {
     function(code, data) {
       // success
       if (providers.isEmpty()) {
-        console.log("No results - is the request set already? ", res);
         res.redirect(CONSTANTS.ERROR_NO_RESULTS);
         return;
       } else {
