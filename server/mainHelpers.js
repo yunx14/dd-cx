1a5a87ef4180f72463496a6dcb4800a9eb75e790
Charles Johnston <cjohnston@delta.org>
2017-07-24 11:56:19 -0700

    api key

--- server/mainHelpers.js
+++ server/mainHelpers.js
@@ -47,7 +47,7 @@ module.exports = {
             { "stylesheet": "http://cloud.typography.com/6549574/670548/css/fonts.css" }
           ],
           "scripts": [
-            {"script": "https://maps.googleapis.com/maps/api/js?key=AIzaSyBwM4PtsUrx03bFU9UhqI44KwdXXqbiGJQ&libraries=places"},
+            {"script": "https://maps.googleapis.com/maps/api/js?client=gme-deltadentalofcalifornia&libraries=places"},
             {"script": CONSTANTS[CONSTANTS.ENVIRONMENT].STATIC_PATH + "jquery.min.js"},
             {"script": CONSTANTS[CONSTANTS.ENVIRONMENT].STATIC_PATH + "main.js"},
             {"script": CONSTANTS[CONSTANTS.ENVIRONMENT].STATIC_PATH + "geocoder.js"},
@@ -177,7 +177,7 @@ module.exports = {
             { "stylesheet": "http://cloud.typography.com/6549574/670548/css/fonts.css" }
           ],
           "scripts": [
-            {"script": "https://maps.googleapis.com/maps/api/js?key=AIzaSyBwM4PtsUrx03bFU9UhqI44KwdXXqbiGJQ&libraries=places"},
+            {"script": "https://maps.googleapis.com/maps/api/js?client=gme-deltadentalofcalifornia&libraries=places"},
             {"script": CONSTANTS[CONSTANTS.ENVIRONMENT].STATIC_PATH + "jquery.min.js"},
             {"script": CONSTANTS[CONSTANTS.ENVIRONMENT].STATIC_PATH + "template3.js"},
             {"script": CONSTANTS[CONSTANTS.ENVIRONMENT].STATIC_PATH + "feedback.js"},
@@ -275,7 +275,7 @@ module.exports = {
             { "stylesheet": "http://cloud.typography.com/6549574/670548/css/fonts.css" }
           ],
           "scripts": [
-            {"script": "https://maps.googleapis.com/maps/api/js?key=AIzaSyBwM4PtsUrx03bFU9UhqI44KwdXXqbiGJQ&libraries=places"},
+            {"script": "https://maps.googleapis.com/maps/api/js?client=gme-deltadentalofcalifornia&libraries=places"},
             {"script": CONSTANTS[CONSTANTS.ENVIRONMENT].STATIC_PATH + "jquery.min.js"},
             {"script": CONSTANTS[CONSTANTS.ENVIRONMENT].STATIC_PATH + "template3.js"},
             {"script": CONSTANTS[CONSTANTS.ENVIRONMENT].STATIC_PATH + "feedback.js"},
@@ -374,7 +374,7 @@ var getListsResults = function(query, req, res) {
         { "stylesheet": "http://cloud.typography.com/6549574/670548/css/fonts.css" }
       ],
       "scripts": [
-        {"script": "https://maps.googleapis.com/maps/api/js?key=AIzaSyBwM4PtsUrx03bFU9UhqI44KwdXXqbiGJQ&libraries=places"},
+        {"script": "https://maps.googleapis.com/maps/api/js?client=gme-deltadentalofcalifornia&libraries=places"},
         {"script": CONSTANTS[CONSTANTS.ENVIRONMENT].STATIC_PATH + "jquery.min.js"},
         {"script": CONSTANTS[CONSTANTS.ENVIRONMENT].STATIC_PATH + "main.js"},
         {"script": CONSTANTS[CONSTANTS.ENVIRONMENT].STATIC_PATH + "feedback.js"},
