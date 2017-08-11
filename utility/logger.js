4a3da1b7ed5fe9031e15ca3ff1d962f1aa2d9ccb
Joanne Xin <JXin@delta.org>
2017-08-10 21:49:14 -0700

    [add] ----

--- utility/logger.js
+++ utility/logger.js
@@ -5,18 +5,28 @@ const CONSTANTS = require("../constants.js"),
 
 module.exports = {
   log: function(message) {
-    logger.log("info", message, Utils.formatTimestamp());
+    logger.log("info", message);
+    logger.log(Utils.formatTimestamp());
+    logger.log("------------------------");
   },
   error: function(message) {
-    logger.log("error", message, Utils.formatTimestamp());
+    logger.log("error", message);
+    logger.log(Utils.formatTimestamp());
+    logger.log("------------------------");
   },
   debug: function(message) {
-    logger.log("debug", message, Utils.formatTimestamp());
+    logger.log("debug", message);
+    logger.log(Utils.formatTimestamp());
+    logger.log("------------------------");
   },
   info: function(message) {
-    logger.log("info", message, Utils.formatTimestamp());
+    logger.log("info", message);
+    logger.log(Utils.formatTimestamp());
+    logger.log("------------------------");
   },
   warn: function(message) {
-    logger.log("warn", message, Utils.formatTimestamp());
+    logger.log("warn", message);
+    logger.log(Utils.formatTimestamp());
+    logger.log("------------------------");
   }
 };
