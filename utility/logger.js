d90e86fbc88ac7fc313e5691340eeb80bc63c100
Joanne Xin <JXin@delta.org>
2017-08-10 21:53:24 -0700

    [remove] -----

--- utility/logger.js
+++ utility/logger.js
@@ -7,26 +7,21 @@ module.exports = {
   log: function(message) {
     logger.log("info", message);
     logger.log(Utils.formatTimestamp());
-    logger.log("------------------------");
   },
   error: function(message) {
     logger.log("error", message);
     logger.log(Utils.formatTimestamp());
-    logger.log("------------------------");
   },
   debug: function(message) {
     logger.log("debug", message);
     logger.log(Utils.formatTimestamp());
-    logger.log("------------------------");
   },
   info: function(message) {
     logger.log("info", message);
     logger.log(Utils.formatTimestamp());
-    logger.log("------------------------");
   },
   warn: function(message) {
     logger.log("warn", message);
     logger.log(Utils.formatTimestamp());
-    logger.log("------------------------");
   }
 };
