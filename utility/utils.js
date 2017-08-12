9669b5f455c5ab41393cc5e5b095849da3528dcc
Robert T Warren <RWarren@delta.org>
2017-08-11 18:09:43 -0700

    cleanup

--- utility/utils.js
+++ utility/utils.js
@@ -167,19 +167,6 @@ module.exports = Utils = {
   },
   formatTimestamp: function() {
     var now = new Date();
-    // return this.pad("                      ",
-    // now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate() + " " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds() + ":" + now.getMilliseconds(),
-    // false);
     return now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate() + " " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds() + ":" + now.getMilliseconds();
-  },
-  // pad: function(pad, str, padLeft) {
-  //   if (typeof str === 'undefined'){
-  //     return pad;
-  //   }
-  //   if (padLeft) {
-  //     return (pad + str).slice(-pad.length);
-  //   } else {
-  //     return (str + pad).substring(0, pad.length);
-  //   }
-  // }
+  }
 };
