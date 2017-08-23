f64ac523cc507be35709f25c5581e0a3f6a04c81
Charles Johnston <cjohnston@delta.org>
2017-08-23 09:59:59 -0700

    throttle

--- public/helpers.js
+++ public/helpers.js
@@ -16,6 +16,30 @@ var FEHelpers = (function(){
   		timeout = setTimeout(later, wait);
   		if (callNow) func.apply(context, args);
   	};
+
+    var throttle = function(fn, threshhold, scope) {
+      threshhold || (threshhold = 250);
+      var last,
+          deferTimer;
+      return function () {
+        var context = scope || this;
+
+        var now = +new Date,
+            args = arguments;
+        if (last && now < last + threshhold) {
+          // hold on to it
+          clearTimeout(deferTimer);
+          deferTimer = setTimeout(function () {
+            last = now;
+            fn.apply(context, args);
+          }, threshhold);
+        } else {
+          last = now;
+          fn.apply(context, args);
+        }
+      };
+    };
+
   };
 
   var fixmystuff = function() {
@@ -39,4 +63,4 @@ var FEHelpers = (function(){
   };
 }());
 
-window.addEventListener("scroll", FEHelpers.debounce(FEHelpers.fixmystuff, 10));
+window.addEventListener("scroll", FEHelpers.throttle(FEHelpers.fixmystuff, 10));
