17ee243f40814e8261883685a819bafd7a920a2a
Charles Johnston <cjohnston@delta.org>
2017-08-23 11:53:26 -0700

    throttle

--- public/helpers.js
+++ public/helpers.js
@@ -16,30 +16,29 @@ var FEHelpers = (function(){
   		timeout = setTimeout(later, wait);
   		if (callNow) func.apply(context, args);
   	};
+  };
 
-    var throttle = function(fn, threshhold, scope) {
-      threshhold || (threshhold = 250);
-      var last,
-          deferTimer;
-      return function () {
-        var context = scope || this;
+  var throttle = function(fn, threshhold, scope) {
+    threshhold || (threshhold = 250);
+    var last,
+        deferTimer;
+    return function () {
+      var context = scope || this;
 
-        var now = +new Date,
-            args = arguments;
-        if (last && now < last + threshhold) {
-          // hold on to it
-          clearTimeout(deferTimer);
-          deferTimer = setTimeout(function () {
-            last = now;
-            fn.apply(context, args);
-          }, threshhold);
-        } else {
+      var now = +new Date,
+          args = arguments;
+      if (last && now < last + threshhold) {
+        // hold on to it
+        clearTimeout(deferTimer);
+        deferTimer = setTimeout(function () {
           last = now;
           fn.apply(context, args);
-        }
-      };
+        }, threshhold);
+      } else {
+        last = now;
+        fn.apply(context, args);
+      }
     };
-
   };
 
   var fixmystuff = function() {
