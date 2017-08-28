2c1062938fb4d27b1ee0d924786f3668dc3b7825
Charles Johnston <cjohnston@delta.org>
2017-08-28 15:00:09 -0700

    map icon

--- public/template3.js
+++ public/template3.js
@@ -16,8 +16,16 @@ function mapLocation(lat, lng) {
 		backgroundColor: '#fff'
 	});
 
+  var markerIcon = {
+    url: "images/map-markers/m1.png",
+    size: new google.maps.Size(20, 30),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(9, 25)
+  };
+
   var marker = new google.maps.Marker({
     position: new google.maps.LatLng(lat, lng),
+    icon: markerIcon,
     map: map
   });
 }
