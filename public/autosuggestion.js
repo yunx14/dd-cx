d2fc2ea41e6727dc81b1325e251729fcfca321ca
Charles Johnston <cjohnston@delta.org>
2017-09-07 15:09:12 -0700

    autosug

--- public/autosuggestion.js
+++ public/autosuggestion.js
@@ -1,4 +1,25 @@
-function getSuggestion(keyword) {
+
+var AutoSuggest = (function() {
+  var init = function(el) {
+    if (el) {
+      var ls = document.getElementById(el);
+      bindToInput(ls);
+    } else {
+      console.log("autosuggest cant bind to input");
+    }
+  }
+
+  var bindToInput = function(el) {
+    el.onkeyup = function() {
+      if (this.value && this.value.length > 2) {
+        getSuggestion(this.value);
+      } else {
+        return;
+      }
+    }
+  };
+
+  var getSuggestion = function(keyword) {
     var endpoint = "//aw-lx0095:19001/providers/suggestion?freeText="+keyword;
 
     var jqxhr = $.getJSON(endpoint)
@@ -10,10 +31,12 @@ function getSuggestion(keyword) {
           // fail siliently
           console.log("shit something went wrong");
         })
-}
+  };
 
-// $("#keyword").keyup(function() {
-//   if (this.value && this.value.length > 2) {
-//     getSuggestion(this.value);
-//   }
-// })
+  return {
+    "init": init
+  };
+}());
+
+var autosuggest_input = "keyword";
+AutoSuggest.init(autosuggest_input);
