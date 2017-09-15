5473af89a0adaf9490ef267a6dc22ab7fccfa5b6
Charles Johnston <cjohnston@delta.org>
2017-09-15 11:10:22 -0700

    passthru

--- server/autoSuggestHelpers.js
+++ server/autoSuggestHelpers.js
@@ -13,9 +13,42 @@ var Logger = require("../utility/logger.js");
 module.exports = {
   passThrough: function(req, res) {
     Logger.log("GET auto suggestions");
-    var auto_text = req.query.text;
-    res.send(200, auto_text);
 
-    // var auto_request = CONSTANTS[CONSTANTS.ENVIRONMENT].SEARCH_SERVICE_PATH + "/suggestions?text=" + keyword;
+    // res.send(200, auto_text);
+
+    var suggestions = new Model();
+    suggestions.query = req.query.text;
+    suggestions.host = CONSTANTS[CONSTANTS.ENVIRONMENT].SEARCH_SERVICE_HOST;
+    suggestions.port = CONSTANTS[CONSTANTS.ENVIRONMENT].SEARCH_SERVICE_PORT;
+    suggestions.path = CONSTANTS[CONSTANTS.ENVIRONMENT].SEARCH_SERVICE_PATH + "/suggestions?text=" + auto_text;
+
+    var promiseData = {
+		res: res,
+	    code: 200,
+	    model: suggestions
+	};
+
+	var handleSuggestions = function(promiseData) {
+        return new Promise(function(resolve, reject) {
+          suggestions.fetch({},
+            function(code, data) {
+              // success
+              if (data) {
+                // send back the data
+                res.send(200, data);
+              }
+              resolve(promiseData);
+            },
+            function(code, data) {
+              // error
+              promiseData.code = code;
+              // send back the error
+              res.send(200, promiseData.code);
+              Logger.warn("ERROR: Failed to request provider: " + promiseData.code);
+              reject(promiseData);
+            }
+          );
+        });
+    };
   }
 };
