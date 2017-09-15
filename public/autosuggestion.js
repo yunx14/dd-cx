189cb343460d91a0b5fa000bc5cc64695a283993
Charles Johnston <cjohnston@delta.org>
2017-09-15 10:38:38 -0700

    jquery to vanilla

--- public/autosuggestion.js
+++ public/autosuggestion.js
@@ -4,16 +4,23 @@ var AutoSuggest = (function() {
       elemValue = "", // value of the input selector
       list = {},  // list of autosuggest items
       selectedIndex = -1, // keep track of selected item
-      nodeEndpoint = "", // this is the url to connect to the db
+      templateId = "",
+      nodeEndpoint = "/autosuggest?text=", // this is the url to connect to the db
       minChars = 3; // minimum number of chars to trigger autosuggest
 
-  var init = function(el) {
+  var init = function(el, template) {
     if (el) {
       boundElem = document.getElementById(el);
       var ls = document.getElementById(el);
       bindToInput(ls);
     } else {
       console.log("autosuggest cant bind to input");
+    }
+
+    if (template) {
+      templateId = template;
+    } else {
+      console.log("no template supplied");
     }
   }
 
@@ -35,7 +42,7 @@ var AutoSuggest = (function() {
           if (list[selectedIndex].dataset.link) {
             window.location.href = list[selectedIndex].dataset.link;
           } else {
-            console.log('submit the search');
+            console.log('submit the search'); //TODO need to write this code still
           }
         } else if (c === 38 || c === 40) { //Up down arrows
           evt.preventDefault();
@@ -122,20 +129,22 @@ var AutoSuggest = (function() {
   };
 
   var close = function() {
+    var container = document.getElementById(templateId);
+
     if (!opened()) {
       return;
     }
 
     // Need to unbind the mouseover events
-    $(".autosuggest-container").remove();
+    container.parentNode.removeChild(container);
     isVisible = false;
     selectedIndex = -1
   };
 
   var open = function(compiledTemplate) {
     if(compiledTemplate) {
-      $("#keyword").after(compiledTemplate); // get keyword from the global vars
-      $(".autosuggest-container").show();
+      boundElem.insertAdjacentHTML('afterend', compiledTemplate);
+      document.getElementById(templateId).style.display = "block";
 
       list = document.getElementById("autosuggest-list").getElementsByTagName("li");
       isVisible = true;
@@ -180,7 +189,7 @@ var AutoSuggest = (function() {
 
     if (keyword && keyword.length >= minChars) {
       // Send the value off to the backend and trigger event on response
-      var endpoint = "http://aw-lx0076:81/find-a-dentist/alpha/autosuggest?text="+keyword;
+      var endpoint = nodeEndpoint+""+keyword;
   
       var jqxhr = $.getJSON(endpoint)
           .done(function(data){
@@ -208,4 +217,4 @@ var AutoSuggest = (function() {
 
 var autosuggest_input = "keyword";
 var autosuggest_template = '<div id="autosuggest-container" class="autosuggest-container" style="display:none"><ul id="autosuggest-list" class="autosuggest-list" role="listbox" aria-activedescendant>{{#if specialties}}<div class="autosuggest__heading"><p>Specialties</p></div>{{#each specialties}}<li class="autosuggest-list__item" id="assp{{@index}}" role="option"><div class="autosuggest-item" ><div class="autosuggest__icon"><i class="icon icon-search-black"></i></div><div class="autosuggest__term"><span class="autosuggest__name">{{name}}</span></div></div></li>{{/each }}{{/if}}{{#if providers}}<div class="autosuggest__heading"><p>Providers</p></div>{{#each providers}}<li class="autosuggest-list__item" role="option" id="aspr{{@index}}" data-link="'+providerDetailsPage+'?providerKey={{providerKey}}&lat={{address.latitude}}&long={{address.longitude}}&location={{address.city}}"><div class="autosuggest-item"><div class="autosuggest__icon"><i class="icon icon-user"></i></div><div class="autosuggest__term"><span class="autosuggest__name">{{firstName}} {{lastName}}</span><span class="autosuggest__specialty">{{specialty}}</span><div class="autosuggest__address"> <span class="autosuggest__address-street">{{address.addressLine}}</span><span class="autosuggest__address-city">{{address.city}}, </span><span class="autosuggest__address-state">{{address.state}}</span></div></div></div></li>{{/each }}{{/if}}{{#if practiceLocations}}<div class="autosuggest__heading"><p>Offices</p></div>{{#each practiceLocations}}<li class="autosuggest-list__item" id="aspl{{@index}}" role="option" data-link="'+officeDetailsPage+'?practiceLocationNumber={{practiceLocationNumber}}&lat={{address.latitude}}&long={{address.longitude}}&location={{address.city}}"><div  class="autosuggest-item" ><div class="autosuggest__icon"><i class="icon icon-office"></i></div> <div class="autosuggest__term"><span class="autosuggest__name">{{officeName}}</span> <div class="autosuggest__address"><span class="autosuggest__address-street">{{address.addressLine}}</span><span class="autosuggest__address-city">{{address.city}}, </span><span class="autosuggest__address-state">{{address.state}}</span></div></div></div></li>{{/each }}{{/if}}{{#if facilities}}<div class="autosuggest__heading"><p>Facilites</p></div>{{#each facilities}}<li class="autosuggest-list__item" id="asfa{{@index}}" role="option" data-link="'+facilityDetailsPage+'?facilityId={{facilityId}}&lat={{address.latitude}}&long={{address.longitude}}&location={{address.city}}"><div  class="autosuggest-item"><div class="autosuggest__icon"><i class="icon icon-office"></i></div><div class="autosuggest__term"><span class="autosuggest__name">{{facilityName}}</span><div class="autosuggest__address"><span class="autosuggest__address-street">{{address.addressLine}}</span><span class="autosuggest__address-city">{{address.city}}, </span><span class="autosuggest__address-state">{{address.state}}</span></div></div></div></li>{{/each }}{{/if}}</ul></div>';
-AutoSuggest.init(autosuggest_input);
+AutoSuggest.init(autosuggest_input, "autosuggest-container");
