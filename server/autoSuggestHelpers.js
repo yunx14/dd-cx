1b290893fa9595a11359355994f4e9cc03405dfa
Joanne Xin <JXin@delta.org>
2017-09-15 17:32:34 -0700

    [clean] auto suggest helper

--- server/autoSuggestHelpers.js
+++ server/autoSuggestHelpers.js
@@ -1,14 +1,10 @@
 const CONSTANTS = require("../constants.js");
-var MainPresenter = require("../views/mainPresenter.js");
-var SolrCollection = require("../collections/solrCollection.js");
 var Model = require("../models/model.js");
-
-var MainTemplate = require("../views/pds.js");
-var AtomicPower = require("../views/atomicPower.js");
-var ViewModel = require("../views/viewModel.js");
-
-var Utils = require("../utility/utils.js");
 var Logger = require("../utility/logger.js");
+
+// var MainTemplate = require("../views/pds.js");
+// var AtomicPower = require("../views/atomicPower.js");
+// var ViewModel = require("../views/viewModel.js");
 
 module.exports = {
   passThrough: function(req, res) {
